dfa5a5118e6e3bcf9dde7155cb73ff6f2c007c8c
pls <notmyproblem@yandex.com>
2023-09-10 15:33:28 -0300

    Added a third trainer card page for Kanto badges Added in battle weather icons
    	modified:   constants/scgb_constants.asm
    	modified:   engine/battle/core.asm
    	modified:   engine/gfx/cgb_layouts.asm
    	modified:   engine/gfx/sgb_layouts.asm
    	modified:   engine/menus/trainer_card.asm
    	deleted:    gfx/trainer_card/badges.png
    	new file:   gfx/trainer_card/johto_badges.pal
    	new file:   gfx/trainer_card/johto_badges.png
    	new file:   gfx/trainer_card/johto_leaders.png
    	new file:   gfx/trainer_card/kanto_badges.pal
    	new file:   gfx/trainer_card/kanto_badges.png
    	new file:   gfx/trainer_card/kanto_leaders.png
    	deleted:    gfx/trainer_card/leaders.png
    	new file:   gfx/weather/rain_icon.png
    	new file:   gfx/weather/sand_icon.png
    	new file:   gfx/weather/sun_icon.png
    	new file:   gfx/weather_images.asm
    	modified:   main.asm

---
+++ gfx/weather_images.asm
@@ -0,0 +1,10 @@
+WeatherImages::
+
+RainWeatherImage:
+INCBIN "gfx/weather/rain_icon.2bpp"
+
+SunWeatherImage:
+INCBIN "gfx/weather/sun_icon.2bpp"
+
+SandstormWeatherImage:
+INCBIN "gfx/weather/sand_icon.2bpp"
