f7d993b66960ac106835f187dd8e753d5b55ed17
pls <magnusopum@hotmail.com>
2025-08-11 14:21:03 -0300

    Adjusted Stat Multipliers to match Evasion

--- data/battle/stat_multipliers.asm
+++ data/battle/stat_multipliers.asm
@@ -5,16 +5,17 @@
 ; once for CalcBattleStats (see engine/battle/effect_commands.asm),
 ; and once for ApplyStatLevelMultiplier (see engine/battle/core.asm).
 
-	db  25, 100 ; -6 =  25%
-	db  28, 100 ; -5 =  28%
-	db  33, 100 ; -4 =  33%
-	db  40, 100 ; -3 =  40%
-	db  50, 100 ; -2 =  50%
-	db  66, 100 ; -1 =  66%
+StatLevelMultipliers:
+	db  33, 100 ; -6 =  33%
+	db  36, 100 ; -5 =  36%
+	db  43, 100 ; -4 =  43%
+	db  50, 100 ; -3 =  50%
+	db  60, 100 ; -2 =  60%
+	db  75, 100 ; -1 =  75%
 	db   1,   1 ;  0 = 100%
-	db  15,  10 ; +1 = 150%
-	db   2,   1 ; +2 = 200%
-	db  25,  10 ; +3 = 250%
-	db   3,   1 ; +4 = 300%
-	db  35,  10 ; +5 = 350%
-	db   4,   1 ; +6 = 400%
+	db 133, 100 ; +1 = 133%
+	db 166, 100 ; +2 = 166%
+	db   2,   1 ; +3 = 200%
+	db 233, 100 ; +4 = 233%
+	db 133,  50 ; +5 = 266%
+	db   3,   1 ; +6 = 300%
