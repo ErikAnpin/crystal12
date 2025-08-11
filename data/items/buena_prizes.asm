f1788c0c4d119e4fae14f514a4642afdb33d5f5d
pls <magnusopum@hotmail.com>
2025-08-11 14:20:43 -0300

    Added Stat Boosting Held Items

--- data/items/buena_prizes.asm
+++ data/items/buena_prizes.asm
@@ -3,10 +3,10 @@ BuenaPrizeItems:
 	db RARE_CANDY,   1
 	db PP_UP,        1
 	db NUGGET,       1
-	db BERSERK_GENE, 2
+	db FOCUS_BAND,   2
 	db KINGS_ROCK,   2
 	db SCOPE_LENS,   2
+	db BRIGHTPOWDER, 2
 	db LEFTOVERS,    3
-	db FOCUS_BAND,   3
 	db MASTER_BALL,  3
 	assert_table_length NUM_BUENA_PRIZES
