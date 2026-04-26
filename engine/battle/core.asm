73fd25920142cb1712a1da77884c37e8b64a4304
pls <magnusopum@hotmail.com>
2026-04-26 13:41:44 -0300

    Moved MoveInfoBox to the right

--- engine/battle/core.asm
+++ engine/battle/core.asm
@@ -5412,34 +5412,34 @@ MoveSelectionScreen:
 	xor a
 	ldh [hBGMapMode], a
 
-	hlcoord 4, 17 - NUM_MOVES - 1
+	hlcoord 0, 17 - NUM_MOVES - 1
 	ld b, 4
 	ld c, 14
 	ld a, [wMoveSelectionMenuType]
 	cp $2
 	jr nz, .got_dims
-	hlcoord 4, 17 - NUM_MOVES - 1 - 4
+	hlcoord 0, 17 - NUM_MOVES - 1 - 4
 	ld b, 4
 	ld c, 14
 .got_dims
 	call Textbox
 
-	hlcoord 6, 17 - NUM_MOVES
+	hlcoord 2, 17 - NUM_MOVES
 	ld a, [wMoveSelectionMenuType]
 	cp $2
 	jr nz, .got_start_coord
-	hlcoord 6, 17 - NUM_MOVES - 4
+	hlcoord 2, 17 - NUM_MOVES - 4
 .got_start_coord
 	ld a, SCREEN_WIDTH
 	ld [wListMovesLineSpacing], a
 	predef ListMoves
 
-	ld b, 5
+	ld b, 1
 	ld a, [wMoveSelectionMenuType]
 	cp $2
 	ld a, 17 - NUM_MOVES
 	jr nz, .got_default_coord
-	ld b, 5
+	ld b, 1
 	ld a, 17 - NUM_MOVES - 4
 
 .got_default_coord
@@ -5489,7 +5489,7 @@ MoveSelectionScreen:
 	jr z, .battle_player_moves
 	dec a
 	jr nz, .interpret_joypad
-	hlcoord 11, 14
+	hlcoord 11, 13
 	ld de, .empty_string
 	call PlaceString
 	jr .interpret_joypad
@@ -5500,7 +5500,7 @@ MoveSelectionScreen:
 	ld a, [wSwappingMove]
 	and a
 	jr z, .interpret_joypad
-	hlcoord 5, 13
+	hlcoord 1, 13
 	ld bc, SCREEN_WIDTH
 	dec a
 	call AddNTimes
@@ -5702,7 +5702,7 @@ MoveInfoBox:
 	xor a
 	ldh [hBGMapMode], a
 
-	hlcoord 0, 8
+	hlcoord 9, 8
 	ld b, 3
 	ld c, 9
 	call Textbox
@@ -5719,7 +5719,7 @@ MoveInfoBox:
 	cp b
 	jr nz, .not_disabled
 
-	hlcoord 1, 10
+	hlcoord 10, 10
 	ld de, .Disabled
 	call PlaceString
 	jr .done
@@ -5755,32 +5755,27 @@ MoveInfoBox:
 	ld [wStringBuffer1], a
 	call .PrintPP
 
-	hlcoord 1, 9
+	hlcoord 10, 9
+	ld a, [wPlayerMoveStruct + MOVE_ANIM]
+	ld b, a
+	predef PrintMoveType
+
+	hlcoord 10, 10
 	ld a, [wPlayerMoveStruct + MOVE_POWER]
 	and a
 	jr z, .no_power
 	
 	ld de, .Power
 	call PlaceString
-	hlcoord 7, 9
+	hlcoord 16, 10
 	ld de, wPlayerMoveStruct + MOVE_POWER
 	lb bc, 1, 3
 	call PrintNum
-	jr .continue_display
+	jr .done
 
 .no_power
 	ld de, .NoPower
 	call PlaceString
-
-.continue_display
-	hlcoord 7, 11
-	ld [hl], "/"
-
-	callfar UpdateMoveData
-	ld a, [wPlayerMoveStruct + MOVE_ANIM]
-	ld b, a
-	hlcoord 2, 10
-	predef PrintMoveType
 
 .done
 	ret
@@ -5791,14 +5786,19 @@ MoveInfoBox:
 	db "POWER:@"
 .NoPower:
 	db "POWER:---@"
+.PPdisplay:
+    db "PP:@"
 
 .PrintPP:
-	hlcoord 5, 11
+	hlcoord 10, 11
 	ld a, [wLinkMode] ; What's the point of this check?
 	cp LINK_MOBILE
 	jr c, .ok
-	hlcoord 5, 11
+	hlcoord 10, 11
 .ok
+	ld de, .PPdisplay
+    call PlaceString
+    hlcoord 13, 11
 	push hl
 	ld de, wStringBuffer1
 	lb bc, 1, 2
@@ -9145,7 +9145,7 @@ GetWeatherImage:
 .WeatherImageOAMData
 ; positions are backwards since
 ; we load them in reverse order
-	db $88, $1c ; y/x - bottom right
-	db $88, $14 ; y/x - bottom left
-	db $80, $1c ; y/x - top right
-	db $80, $14 ; y/x - top left
+	db $90, $98 ; bottom right
+	db $90, $90 ; bottom left
+	db $88, $98 ; top right
+	db $88, $90 ; top left
